a6247ecf65d9cfcd456c6d4c96dfc24e92b8f52b
sunjie <hud35138@163.com>
2018-11-24 08:12:04 +0800

    编码规范

--- python_learn/python_learn_3.py
+++ python_learn/python_learn_3.py
@@ -1,12 +1,13 @@
-'''
+"""
 Author ：SunJie
 这次，我希望加入unittest单元测试框架来运行
-'''
-from selenium import webdriver #下载了selenium之后，我可以把selenium导入进来
+"""
+from selenium import webdriver  # 下载了selenium之后，我可以把selenium导入进来
 import unittest
 
-class Mytest(unittest.TestCase): #写个类，继承unnitest.Testcase
-    '''
+
+class Mytest(unittest.TestCase):  # 写个类，继承unnitest.Testcase
+    """
     生命周期按照下列方式排序
     setUp
     testOne
@@ -15,23 +16,19 @@ class Mytest(unittest.TestCase): #写个类，继承unnitest.Testcase
     setUp
     testTwo
     teardown
-    '''
+    """
 
     def setUp(self):
-        self.my_window = webdriver.Chrome() #申明了一个chrome浏览器
-        self.my_window.get("http://192.168.14.103:8083/osf-pvs-mock/ftl/requestApplyOsf.jsp") #用浏览器打开测试的网站
-
+        self.my_window = webdriver.Chrome()  # 申明了一个chrome浏览器
+        self.my_window.get("http://192.168.14.103:8083/osf-pvs-mock/ftl/requestApplyOsf.jsp")  # 用浏览器打开测试的网站
 
     def tearDown(self):
-        self.my_window.close() #关闭浏览器
+        self.my_window.close()  # 关闭浏览器
 
     def test_a_one(self):
         membercode = self.my_window.find_element_by_name('membercode')
         membercode.clear()
         membercode.send_keys('123456')
-
-
-
 
 
 if __name__ == '__main__':
--- python_learn/python_learn_4.py
+++ python_learn/python_learn_4.py
@@ -1,8 +1,8 @@
-'''
+"""
 Author ：SunJie
 在3里面，我写了一个testcase，但是我可能会有一堆testcase一起跑
 所以，我这边用testsutie（testcase容器），然后直接跑这个testsuite
-'''
+"""
 
 import unittest
 from python_learn.python_learn_3 import Mytest
@@ -10,11 +10,12 @@ from python_learn.python_learn_3_copy import Mytest as Mytest_copy
 
 
 def suite():
-    suite = unittest.TestSuite()#申明一个testsuite
-    suite.addTest(Mytest('test_a_one'))#把第一个测试case添加进testsuite
-    suite.addTest(Mytest_copy('test_a_one_copy'))#把第二个测试case添加进testsuite
-    return suite
+    test_suite = unittest.TestSuite()  # 申明一个testsuite
+    test_suite.addTest(Mytest('test_a_one'))  # 把第一个测试case添加进testsuite
+    test_suite.addTest(Mytest_copy('test_a_one_copy'))  # 把第二个测试case添加进testsuite
+    return test_suite
+
 
 if __name__ == '__main__':
     runner = unittest.TextTestRunner()
-    runner.run(suite())#运行suite
+    runner.run(suite())  # 运行suite
